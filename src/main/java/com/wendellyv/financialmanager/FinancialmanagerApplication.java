77446e35c78f0431ee4fce47771d956c356a1507
Wendell Viani <wendellylan@gmail.com>
2026-01-07 12:00:07 -0300

    Project created

---
+++ src/main/java/com/wendellyv/financialmanager/FinancialmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.wendellyv.financialmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wendellyv/financialmanager/FinancialmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wendellyv.financialmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
