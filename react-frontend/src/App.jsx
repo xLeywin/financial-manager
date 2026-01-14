9bff1651b6c931f1ef34a4fed26e83a191a1e7cd
Wendell Ylan Viani <wendellylan@gmail.com>
2026-01-14 09:33:03 -0300

    Setting table and form

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -1,25 +1,42 @@
-import { useEffect, useState } from 'react';
+  import { useEffect, useState } from 'react';
 import { getIncomes } from './services/api';
+import Form from './components/Form';
+import Table from './components/Table';
+import './App.css';
 
 function App() {
+  // UseStage
+  const [btnRegister, setBtnRegister] = useState(true);
+  const [users, setUser] = useState([]);
   const [incomes, setIncomes] = useState([]);
+  const [expenses, setExpenses] = useState([]);
 
+  // UseEffect
   useEffect(() => {
-    getIncomes().then(setIncomes);
+    fetch("http://localhost:8080/users")
+    .then(returnU => returnU.json())
+    .then(converted_return => setUser(converted_return));
+
   }, []);
 
   return (
-    <div>
-      <h1>Incomes</h1>
-      <ul>
+    <div className="App">
+      <div className="container">
+        <p>{JSON.stringify(users)}</p>
+        <Form button={btnRegister}/>
+        <Table />
+      </div>
+    </div>
+  );
+  /*
+  <ul>
         {incomes.map(income => (
           <li key={income.id}>
             {income.description} - {income.amount}
           </li>
         ))}
       </ul>
-    </div>
-  );
+  */
 }
 
 export default App;
