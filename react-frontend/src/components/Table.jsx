7628806c71c6bec7662deb31745ad210334c0061
Wendell Ylan Viani <wendellylan@gmail.com>
2026-01-14 09:33:03 -0300

    Setting table and form

---
+++ react-frontend/src/components/Table.jsx
@@ -0,0 +1,26 @@
+function Table(){
+    return(
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Título</th>
+                    <th>Tipo</th>
+                    <th>Categoria</th>
+                    <th>Valor</th>
+                    <th>Selecionar</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    )
+}
+
+export default Table;
